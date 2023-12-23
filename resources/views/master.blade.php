f22badd662c019a2863c146658273d8250b1787f
Code With Sabbir <tauhid10030@gmail.com>
2023-12-23 21:06:03 +0600

    space added

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,7 +9,7 @@
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}"> 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('frontend')}}/assets/images.ico">
     <!-- CSS
